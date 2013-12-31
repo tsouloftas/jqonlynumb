cf021bd67b622535686179f5fd448636981e2a8b
Alex Tsouloftas <i@alex.ms>
2013-12-31 11:59:14 +0200

    jQuery Tiny Only numbers validator plugin

--- onlynumbers.js
+++ onlynumbers.js
@@ -1,7 +1,8 @@
 var cctime;
-function onumcallback() {
- 	// callback   
+function onlynumbcallback() {
+    //callback
 }
+
 function ccchange() {
     ccenter(this)
 }
@@ -9,15 +10,15 @@ function ccchange() {
 function vn(a) {
     var b = window.event ? a.keyCode : a.which;
     if (8 == a.keyCode || 46 == a.keyCode || 37 == a.keyCode || 39 == a.keyCode) return !0;
-    if (48 > b || 57 < b || 222 == a.keyCode) return $(".numbers").css("border", "solid 1px #ab3434", 300, cb), !1;
+    if (48 > b || 57 < b || 222 == a.keyCode) return $(this).css("border", "solid 1px #ab3434", 300, cb(this)), !1;
     clearTimeout(cctime);
-    //cctime = setTimeout(onumcallback, 250);
+    //cctime = setTimeout(onlynumbcallback, 250);
     return !0
 }
 
-function cb() {
+function cb(th) {
     setTimeout(function () {
-        $(".numbers").removeClass("unable").css("border", "1px solid #bbb")
+        $(th).removeClass("unable").css("border", "1px solid #bbb")
     }, 100)
 }
 
